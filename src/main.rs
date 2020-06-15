71cf614db31b9957fcc60bc5ddaf6e679722af73
Staneesh <blkreasons@outlook.com>
2020-06-15 12:40:34 +0200

    decided on offscreen rendering

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-extern crate sdl2;
+extern crate image;
 
 fn main() {
-    println!("Hello, world!");
+
+    let buffer: &[u8] = unimplemented!(); // Generate the image data
+
+    // Save the buffer as "image.png"
+    image::save_buffer("image.png", buffer, 800, 600, image::ColorType::Rgb8).unwrap()
 }
