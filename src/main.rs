9fe23e75d0997c42307a28111baf23c79768f243
Staneesh <blkreasons@outlook.com>
2020-06-16 22:49:13 +0200

    Added camera struct along with constructors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 extern crate image;
 
 use image::{ImageBuffer, RgbImage};
-use glam::{Vec3};
+use glam::{Vec3, Vec2};
 
 #[derive(Debug)]
 struct Ray
 {
     position: Vec3,
     direction: Vec3,
+}
+
+#[derive(Debug)]
+struct Camera
+{
+    position: Vec3,
+    direction: Vec3,
+    canvas_dimensions: Vec2,
+    canvas_distance: f32,
 }
 
 impl Ray
@@ -25,8 +34,8 @@ impl Ray
     {
         Ray
         {
-            position: Vec3::new(1.0, 0.0, 0.0),
-            direction: Vec3::new(0.0, 0.0, -1.0),
+            position: Vec3::new(0.0, 0.0, 0.0),
+            direction: Vec3::new(0.0, 0.0, 0.0),
         }
     }
 
@@ -45,13 +54,51 @@ impl Ray
     }
 }
 
+impl Camera
+{
+    fn init(position: Vec3, direction: Vec3,
+            canvas_dimensions: Vec2, canvas_distance: f32) -> Camera
+    {
+        Camera
+        {
+            position, direction,
+            canvas_dimensions, canvas_distance, 
+        }
+    }
+
+    fn new() -> Camera
+    {
+        Camera
+        {
+            position: Vec3::new(2.0, 0.0, 0.0),
+            direction: Vec3::new(0.0, 0.0, -1.0),
+            canvas_dimensions: Vec2::new(1.0, 1.0),
+            canvas_distance: 1.0,
+        }
+    }
+
+    fn from(camera: Camera) -> Camera
+    {
+        Camera
+        {
+            position: camera.position,
+            direction: camera.direction,
+            canvas_dimensions: camera.canvas_dimensions,
+            canvas_distance: camera.canvas_distance,
+        }
+    }
+
+}
+
 fn main()
 {
     let width = 512;
     let height = 512;
+    let aspect_ratio = width as f32 / height as f32;
+
     let mut img: RgbImage = ImageBuffer::new(width, height);
 
-
+      
 
 
     for pixel in img.pixels_mut() 
